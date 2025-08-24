e936ec87bd00ce072f56591419a193802dff484c
Hongwei Yang <yhw1639@gmail.com>
2025-08-24 10:22:35 -0700

    Added simple test codes

--- src/TokenRecordAgent.test.js
+++ src/TokenRecordAgent.test.js
@@ -1,7 +1,15 @@
+import assert from 'node:assert';
+import test from 'node:test';
 
 import TokenRecordAgent from './TokenRecordAgent.js';
 
-let agent = new TokenRecordAgent();
-let t = agent.initFor('abc');
-console.log(t);
-console.log(agent.pop(t));
+test.describe('TokenRecordAgent test', () => {
+  test.it('init and pop', () => {
+    let agent = new TokenRecordAgent();
+    let userId = 'abc';
+    let t = agent.initFor(userId);
+    assert.ok(t.length > 0);
+    assert.equal(agent.pop(t), userId);
+    assert.equal(agent.pop(t), null);
+  });
+});
